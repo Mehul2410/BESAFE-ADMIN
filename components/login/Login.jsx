4585140bcbb4a97ca056eddfa876b6c0d156ef22
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-12-07 16:01:38 +0530

    private route don

--- components/login/Login.jsx
+++ components/login/Login.jsx
@@ -1,14 +1,48 @@
 import forms from "@tailwindcss/forms";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Login = () => {
+  const router = useRouter();
+  const [loginDetail, setLoginDetail] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  async function loginFun() {
+    try {
+      const res = await fetch("http://localhost:8000/admin-sign-in", {
+        method: "post",
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(loginDetail),
+      });
+      const result = await res.json();
+      console.log(result);
+      if (result.success === true) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(loginDetail);
   return (
     <div className="flex justify-center item-center mt-36">
-      <form className="bg-darker flex flex-col py-10  w-80 h-64 space-y-6 rounded-xl drop-shadow-xl items-center">
+      <form
+        className="bg-darker flex flex-col py-10  w-80 h-64 space-y-6 rounded-xl drop-shadow-xl items-center"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
           type="email"
           name="email"
           placeholder="Email"
+          value={loginDetail.email}
+          onChange={(e) => setLoginDetail({ email: e.target.value })}
           required
           className="bg-light placeholder-white rounded-lg w-60"
         />
@@ -16,6 +50,9 @@ const Login = () => {
           type="password"
           name="password"
           placeholder="Password"
+          onChange={(e) =>
+            setLoginDetail({ ...loginDetail, password: e.target.value })
+          }
           required
           className="bg-light placeholder-white rounded-lg w-60"
         />
@@ -24,6 +61,7 @@ const Login = () => {
           name="Login"
           value="Login"
           className=" rounded-md h-10 w-48 flex justify-center cursor-pointer bg-dark text-white font-medium"
+          onClick={loginFun}
         />
       </form>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BasePage from "../layout/BasePage";
 import BaseLayout from "../layout/BaseLayout";
 // import Login from "../components/login/Login";
+import { parseCookies } from "nookies";
 import Verification from "../components/verification/Verification";
 
-const index = () => {
+const index = ({ adminLogin }) => {
+  console.log(adminLogin);
   return (
     <div>
       <BaseLayout>
@@ -15,4 +17,19 @@ const index = () => {
     </div>
   );
 };
+
+export const getServerSideProps = async (ctx) => {
+  const { adminLogin } = parseCookies(ctx);
+
+  if (!adminLogin) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
+};
+
 export default index;
