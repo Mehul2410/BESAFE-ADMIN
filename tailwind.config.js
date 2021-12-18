9aa2a2efe2b7b82cc2f0adda5caf73986a432acd
vallabh2920 <vallabhingle2920@gmail.com>
2021-12-18 19:12:06 +0530

    screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./pages/**/*.{js,jsx}",
     "./components/**/*.{js,jsx}",
     "./Layout/**/*.{js,jsx}",
