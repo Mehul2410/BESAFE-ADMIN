2b04e89b3b035daa08e04edf6a329bc028789088
Mehul Gawhale <mehulgawhale18hc@student.mes.ac.in>
2021-12-18 18:01:56 +0530

    Update BaseLayout.jsx

--- layout/BaseLayout.jsx
+++ layout/BaseLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 const BaseLayout = ({ children, authstatus }) => {
   return (
-    <div className="bg-darkest h-full overflow-y-scroll scrollbar-none">
+    <div className="bg-darkest h-screen overflow-y-scroll scrollbar-none">
       <Header authstatus={authstatus} />
 
       {children}
