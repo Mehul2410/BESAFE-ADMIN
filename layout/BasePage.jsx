d78cfaa6e484f7bd0d832ef23ed1a6ae3d50c268
Mehul Gawhale <mehulgawhale18hc@student.mes.ac.in>
2021-12-18 17:57:05 +0530

    Update BasePage.jsx

--- layout/BasePage.jsx
+++ layout/BasePage.jsx
@@ -4,7 +4,7 @@ const BasePage = (props) => {
   const { children, className = "" } = props;
   return (
     <div className={`base-page h-screen ${className}`} >
-      <div className="max-w-7xl m-auto h-full px-5 relative">{children}</div>
+      <div className="max-w-7xl m-auto h-screen px-5 relative">{children}</div>
     </div>
   );
 };
