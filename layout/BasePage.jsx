e284b576c00f1d5598e94dbdc3fc5e283c7289f2
Mehul Gawhale <mehulgawhale18hc@student.mes.ac.in>
2021-12-18 17:53:52 +0530

    Update BasePage.jsx

--- layout/BasePage.jsx
+++ layout/BasePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BasePage = (props) => {
   const { children, className = "" } = props;
   return (
-    <div className={`base-page h-full ${className}`} >
+    <div className={`base-page h-screen ${className}`} >
       <div className="max-w-7xl m-auto h-full px-5 relative">{children}</div>
     </div>
   );
