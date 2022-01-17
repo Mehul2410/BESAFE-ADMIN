39cbe58aeded789f3a04c0430a5cded15acacce4
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2022-01-18 02:07:04 +0530

    login fix

--- components/login/Login.jsx
+++ components/login/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
       const res = await fetch("https://besaferestapi.herokuapp.com/api/admin", {
         method: "POST",
-        mode: "cors",
+        mode: "no-cors",
         credentials: "include",
         headers: headers,
         body: JSON.stringify(loginDetail),
