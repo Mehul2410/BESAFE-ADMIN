489ad4fe0a5f23ac65639e315e1acca89bafc3e3
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2022-03-31 22:34:40 +0530

    error fi

--- components/verification/VerificationTable.jsx
+++ components/verification/VerificationTable.jsx
@@ -24,141 +24,147 @@ function PoliceRows({ person, setView }) {
 	const [edit, setEdit] = React.useState(false);
 	const [undo, setUndo] = React.useState("");
 
-	return person.userDetails && (
-		<tr key={person.id}>
-			<td className='px-6 py-4 whitespace-nowrap'>
-				<div className='flex items-center'>
-					<div className='flex-shrink-0 h-10 w-10'>
-						<img
-							className='h-10 w-10 rounded-full'
-							src={person.avatar ? person.avatar : "./police.svg"}
-							alt=''
-						/>
-					</div>
-					<div className='ml-4'>
-						{/* <div className="text-sm font-medium text-light">
+	return (
+		person.userDetails && (
+			<tr key={person.id}>
+				<td className='px-6 py-4 whitespace-nowrap'>
+					<div className='flex items-center'>
+						<div className='flex-shrink-0 h-10 w-10'>
+							<img
+								className='h-10 w-10 rounded-full'
+								src={person.avatar ? person.avatar : "./police.svg"}
+								alt=''
+							/>
+						</div>
+						<div className='ml-4'>
+							{/* <div className="text-sm font-medium text-light">
       {person.name}
     </div> */}
-						<div className='text-sm font-medium text-light'>
-							<input
-								type='text'
-								defaultValue={person.name}
-								value={person.name}
-								className='bg-dark outline-none border-0 p-0'
-								onChange={(e) => {
-									person.name = e.target.value;
-									setText([...text]);
-									setUndo(e.target.defaultValue);
-								}}
-								disabled={edit ? false : true}
-							></input>
-						</div>
-						{/* <div className="text-sm text-light">
+							<div className='text-sm font-medium text-light'>
+								<input
+									type='text'
+									defaultValue={person.name}
+									value={person.name}
+									className='bg-dark outline-none border-0 p-0'
+									onChange={(e) => {
+										person.name = e.target.value;
+										setText([...text]);
+										setUndo(e.target.defaultValue);
+									}}
+									disabled={edit ? false : true}
+								></input>
+							</div>
+							{/* <div className="text-sm text-light">
     {person.email}
   </div> */}
-						<div className='text-sm text-light'>
-							<input
-								type={text}
-								value={person.email}
-								className='bg-dark text-sm outline-none border-0  p-0'
-								onChange={(e) => {
-									person.email = e.target.value;
-									setText([...text]);
-								}}
-								disabled={edit ? false : true}
-							></input>
+							<div className='text-sm text-light'>
+								<input
+									type={text}
+									value={person.email}
+									className='bg-dark text-sm outline-none border-0  p-0'
+									onChange={(e) => {
+										person.email = e.target.value;
+										setText([...text]);
+									}}
+									disabled={edit ? false : true}
+								></input>
+							</div>
 						</div>
 					</div>
-				</div>
-			</td>
-			<td className='px-6 py-4 whitespace-nowrap  '>
-				<div className='text-base  text-light text-center px-3 py-2  rounded-xl bg-darkest'>
-					<button
-						onClick={() => {
-							setView({
-								state: true,
-								url: person.userDetails?.verificationPaper,
-							});
-						}}
-					>
-						View
-					</button>
-				</div>
-			</td>
-			<td className='px-4 py-4 whitespace-nowrap'>
-				{/* <div className="text-sm text-light text-center ">
+				</td>
+				<td className='px-6 py-4 whitespace-nowrap  '>
+					<div className='text-base  text-light text-center px-3 py-2  rounded-xl bg-darkest'>
+						<button
+							onClick={() => {
+								setView({
+									state: true,
+									url: person.userDetails?.verificationPaper,
+								});
+							}}
+						>
+							View
+						</button>
+					</div>
+				</td>
+				<td className='px-4 py-4 whitespace-nowrap'>
+					{/* <div className="text-sm text-light text-center ">
   {person.post}
 </div> */}
-				<div className='text-sm text-light text-center '>
-					<input
-						type='text'
-						value={person.userDetails?.policePost}
-						className='bg-dark text-sm outline-none border-0 text-center p-0'
-						onChange={(e) => {
-							person.post = e.target.value;
-							setText([...text]);
-						}}
-						disabled={edit ? false : true}
-					></input>
-				</div>
-			</td>
-			<td className='px-4 py-4 whitespace-nowrap'>
-				{/* <div className="text-sm text-light text-center">
+					<div className='text-sm text-light text-center '>
+						<input
+							type='text'
+							value={person.userDetails?.policePost}
+							className='bg-dark text-sm outline-none border-0 text-center p-0'
+							onChange={(e) => {
+								person.post = e.target.value;
+								setText([...text]);
+							}}
+							disabled={edit ? false : true}
+						></input>
+					</div>
+				</td>
+				<td className='px-4 py-4 whitespace-nowrap'>
+					{/* <div className="text-sm text-light text-center">
   {person.area}
 </div> */}
-				<div className='text-sm text-light text-center'>
-					<input
-						type='text'
-						value={person.userDetails?.postingArea}
-						className='bg-dark text-sm outline-none border-0 text-center p-0'
-						onChange={(e) => {
-							person.area = e.target.value;
-							setText([...text]);
-						}}
-						disabled={edit ? false : true}
-					></input>
-				</div>
-			</td>
-			<td className='px-6 py-4 whitespace-nowrap'>
-				<div className='text-sm text-light text-center'></div>
-			</td>
+					<div className='text-sm text-light text-center'>
+						<input
+							type='text'
+							value={person.userDetails?.postingArea}
+							className='bg-dark text-sm outline-none border-0 text-center p-0'
+							onChange={(e) => {
+								person.area = e.target.value;
+								setText([...text]);
+							}}
+							disabled={edit ? false : true}
+						></input>
+					</div>
+				</td>
+				<td className='px-6 py-4 whitespace-nowrap'>
+					<div className='text-sm text-light text-center'></div>
+				</td>
 
-			<td className=' py-4 whitespace-nowrap'>
-				<div className='text-sm text-light text-center'>
-					<ActiveInactive edit={edit} />
-				</div>
-			</td>
-			<td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
-				<div className='space-x-2'>
-					{edit === true ? (
+				<td className=' py-4 whitespace-nowrap'>
+					<div className='text-sm text-light text-center'>
+						<ActiveInactive edit={edit} />
+					</div>
+				</td>
+				<td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
+					<div className='space-x-2'>
+						{edit === true ? (
+							<button
+								className='bg-light text-sm rounded-full p-3'
+								onClick={() => {
+									setEdit(false);
+								}}
+							>
+								<img
+									src='/close.svg'
+									alt='close'
+									className='h-5 w-5 bg-light '
+								/>
+							</button>
+						) : (
+							<button
+								className='bg-light text-sm rounded-full p-3'
+								onClick={() => {
+									setEdit(true);
+								}}
+							>
+								<img src='/edit.svg' alt='edit' className='h-5 w-5 bg-light ' />
+							</button>
+						)}
 						<button
-							className='bg-light text-sm rounded-full p-3'
-							onClick={() => {
-								setEdit(false);
-							}}
+							className='bg-light rounded-full p-3'
+							onClick={() => setEdit(false)}
 						>
-							<img src='/close.svg' alt='close' className='h-5 w-5 bg-light ' />
+							<img src='/save.svg' alt='save' className='h-5 w-5 bg-light ' />
 						</button>
-					) : (
-						<button
-							className='bg-light text-sm rounded-full p-3'
-							onClick={() => {
-								setEdit(true);
-							}}
-						>
-							<img src='/edit.svg' alt='edit' className='h-5 w-5 bg-light ' />
-						</button>
-					)}
-					<button
-						className='bg-light rounded-full p-3'
-						onClick={() => setEdit(false)}
-					>
-						<img src='/save.svg' alt='save' className='h-5 w-5 bg-light ' />
-					</button>
-				</div>
-			</td>
-		</tr>
-	)}
+					</div>
+				</td>
+			</tr>
+		)
+	);
 }
 
 const VerificationTable = ({ police, setView }) => {
